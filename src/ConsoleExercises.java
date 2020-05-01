c048a6842c1c08662a86ee0af931c2b48ce4905c
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-01 15:33:52 -0500

    finished exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %s\n", pi);
+        System.out.printf("The value of pi is approximately %f%n", pi);
 
         Scanner scanner = new Scanner(System.in);
-//        // User enters 1 number
-//        System.out.print("Enter a number: ");
-//        int userNumber = scanner.nextInt();
-//        System.out.printf("You entered: %s\n", userNumber);
-//
-//        // User enters 3 consecutive words
-//        System.out.print("Enter 3 words: \n");
-//        String userWordOne = scanner.next();
-//        String userWordTwo = scanner.next();
-//        String userWordThree = scanner.next();
-//        System.out.printf("You entered: %s %s %s\n", userWordOne, userWordTwo, userWordThree);
+        // User enters 1 number
+        System.out.print("Enter a number: ");
+        int userNumber = scanner.nextInt();
+        System.out.printf("You entered: %d%n", userNumber);
+
+        // User enters 3 consecutive words
+        System.out.print("Enter 3 words: \n");
+        String userWordOne = scanner.next();
+        String userWordTwo = scanner.next();
+        String userWordThree = scanner.next();
+        System.out.printf("You entered: %s %s %s\n", userWordOne, userWordTwo, userWordThree);
 
         // User enters a sentence
 
-//        System.out.print("Enter a sentence: ");
-//        String userSentence = scanner.nextLine();
-//        System.out.printf("You entered: %s\n", userSentence);
+        System.out.print("Enter a sentence: \n");
+        String userSentence = scanner.nextLine();
+        System.out.printf("You entered: %s\n", userSentence);
 
+        // Classroom size
         System.out.print("Enter a length and then width: ");
         int userLength = scanner.nextInt();
         int userWidth = scanner.nextInt();
