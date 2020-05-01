1bd9bb27612312dbe496b1332ecf073a445e004e
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-01 14:04:08 -0500

    completed numbers, words, sentences questions

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,13 +6,23 @@ public class ConsoleExercises {
         System.out.printf("The value of pi is approximately %s\n", pi);
 
         Scanner scanner = new Scanner(System.in);
+//        // User enters 1 number
+//        System.out.print("Enter a number: ");
+//        int userNumber = scanner.nextInt();
+//        System.out.printf("You entered: %s\n", userNumber);
+//
+//        // User enters 3 consecutive words
+//        System.out.print("Enter 3 words: \n");
+//        String userWordOne = scanner.next();
+//        String userWordTwo = scanner.next();
+//        String userWordThree = scanner.next();
+//        System.out.printf("You entered: %s %s %s\n", userWordOne, userWordTwo, userWordThree);
 
-        System.out.print("Enter a number: ");
-        int userNumber = scanner.nextInt();
-        System.out.printf("You entered: %s", userNumber);
+        // User enters a sentence
 
-
-
+        System.out.print("Enter a sentence: ");
+        String userSentence = scanner.nextLine();
+        System.out.printf("You entered: %s\n", userSentence);
 
 
     }
