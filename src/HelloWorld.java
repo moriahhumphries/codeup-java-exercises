a6963b4c43145cc19d0d948d98c0a436b5d43ee5
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-01 11:18:56 -0500

    completed syntax, types, and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,51 @@ public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
         System.out.print("Hello ");
-        System.out.print("World!");
+        System.out.print("World!\n");
+        int myFavoriteNumber = 37;
+        System.out.println(myFavoriteNumber);
+        String myString = "Moriah";
+        System.out.println(myString);
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+        // post increment
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        // pre increment
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        // reserved word
+//        final class =
+        // variable used twice
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+        // 9
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+
+        // 12
+//        int x = 3;
+//        x *= 4;
+//
+//        System.out.println(x);
+        // -3?
+         int x = 10;
+         int y = x /= 2;
+         x -= y;
+//         x = x / y;
+//         y = y - x;
+//        System.out.println(x);
+
+        int testNumber = Integer.MAX_VALUE;
+        testNumber++;
+        System.out.println(testNumber);
     }
 }
