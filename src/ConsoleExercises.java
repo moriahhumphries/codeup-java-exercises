1781525caa233835b86b698f71e821b4f2c85216
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-01 12:25:49 -0500

    updated user number prompt question

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %s", pi);
+        System.out.printf("The value of pi is approximately %s\n", pi);
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        String userInput = scanner.next();
-
-        System.out.printf("You entered: %s", userInput);
+        int userNumber = scanner.nextInt();
+        System.out.printf("You entered: %s", userNumber);
 
 
 
