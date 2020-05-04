1b7338feec0a654022d9672928125eae3eec135e
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-04 11:57:18 -0500

    completed fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,15 @@ import java.sql.SQLOutput;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 5; i <= 15; i++) {
+        for(int i = 1; i <= 100; i++) {
             System.out.println("Number: " + i);
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz: " + i);
+            } else if (i % 3 == 0)  {
+                System.out.println("Fizz: " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz: " + i);
+            }
         }
 
     }
