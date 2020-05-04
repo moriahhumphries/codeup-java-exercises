b41bdc2a2bb3a6159807bc67e40112cf91caa165
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-04 11:42:32 -0500

    completed for loop 5-15 by 1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(long i = 2; i < 1000000; i*=i) {
+        for(int i = 5; i <= 15; i++) {
             System.out.println("Number: " + i);
         }
 
