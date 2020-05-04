df30ec90d2ae2a3f107ed30c9556462f524a5508
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-04 11:39:52 -0500

    completed for loop 100 - -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i+=2) {
+        for(int i = 100; i >= -10; i-=5) {
             System.out.println("Number: " + i);
         }
 
