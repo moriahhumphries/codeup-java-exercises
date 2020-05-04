a80385e12e78ba8051d4e362519058d74b021c74
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-04 14:35:01 -0500

    completed data table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,14 +4,24 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int userNumber = scanner.nextInt();
+        int userNumber = Integer.parseInt(scanner.nextLine());
         System.out.print("You entered: " + userNumber + "\n");
         System.out.print("Here is your table!\nnumber | squared | cubed\n");
-        for(int i = 1; i <= userNumber; i++) {
-            int squared = i*=i;
-            int cubed = i*=i*=i;
-            System.out.print(i + "\t" + "\t" + "\t" + squared + "\t" + "\t" + "\t" + cubed + "\n");
-        }
 
+        for (int i = 1; i <= userNumber; i++) {
+                int squared = i * i;
+                int cubed = i * squared;
+                System.out.print(i + "\t" + "\t" + "\t" + squared + "\t" + "\t" + "\t" + cubed + "\n");
+        }
+        System.out.print("Would you like to continue: Yes or No? ");
+        String answer = scanner.nextLine();
+        boolean yes = true;
+        boolean no = false;
+
+        if (answer.equals("yes") || answer.equals("Yes")) {
+            System.out.print("Continuing!");
+        } else {
+            System.out.print("Program Finished.");
+        }
     }
 }
