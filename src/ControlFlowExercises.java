93d132ddc4d39c3a06bc78ba230fa50b6a8cda78
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-04 12:19:48 -0500

    updated table styling and alignment

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer: \n");
+        System.out.print("Enter an integer: ");
         int userNumber = scanner.nextInt();
         System.out.print("You entered: " + userNumber + "\n");
+        System.out.print("Here is your table!\nnumber | squared | cubed\n");
         for(int i = 1; i <= userNumber; i++) {
-            System.out.print("Counting from 1 to your number: " + i + "\n");
+            System.out.print(i + "\t" + "\t" + "\t" + i + "\t" + "\t" + "\t" + i + "\n");
         }
+
     }
 }
