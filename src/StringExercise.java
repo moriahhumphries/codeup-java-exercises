dc6f8ed899e755171444c03e142cb3d8003846cb
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-05 11:51:56 -0500

    completed bob exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -18,29 +18,29 @@ public class StringExercise {
 
 
         // Bob
-//        Scanner scanner = new Scanner(System.in);
-//        String answer = "";
-//        do {
-//            System.out.print("Enter a numerical grade from 0 - 100: ");
-//            int userGrade = Integer.parseInt(scanner.nextLine());
-//            System.out.print("You entered: " + userGrade + "\n");
+        Scanner scanner = new Scanner(System.in);
+        String answer = "";
+        do {
+        System.out.print("Say something to Bob: ");
+        String userInput = scanner.nextLine().trim();
+        System.out.print("You entered: " + userInput + "\n");
 //
-//            if (answer.) {
-//                System.out.print("You got an A!\n");
-//            } else if (userGrade >= 80) {
-//                System.out.print("You got a B!\n");
-//            } else if (userGrade >= 67) {
-//                System.out.print("You got a C.\n");
-//            } else if (userGrade >= 60) {
-//                System.out.print("Well, you got a D.\n");
-//            } else {
-//                System.out.print("It's an F, sorry bud.\n");
-//            }
-//
-//            System.out.print("Would you like to continue: Yes or No? ");
-//            answer = scanner.nextLine();
-//
-//        } while (answer.equalsIgnoreCase("Yes"));
+        if (userInput.endsWith("?")) {
+            System.out.print("Sure\n");
+        } else if (userInput.endsWith("!")) {
+            System.out.print("Whoa, chill out!\n");
+        } else if (userInput.equals("")) {
+            System.out.print("Fine, be that way!\n");
+        } else {
+            System.out.print("Whatever\n");
+            }
 
+            System.out.print("Would you like to continue: Yes or No? ");
+            answer = scanner.nextLine();
+
+
+
+        }while (answer.equalsIgnoreCase("Yes"));
     }
 }
+
