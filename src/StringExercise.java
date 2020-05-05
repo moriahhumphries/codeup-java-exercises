40763e922fa08879d5fb677a11dc167652f495f6
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-05 11:40:13 -0500

    completed printing string exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        String brick = "We don't need no education\nWe don't need no thought control";
+
+        System.out.println(brick);
+
+        String inside = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(inside);
+//
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+//
+        String slashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(slashes);
+
+
+        // Bob
+//        Scanner scanner = new Scanner(System.in);
+//        String answer = "";
+//        do {
+//            System.out.print("Enter a numerical grade from 0 - 100: ");
+//            int userGrade = Integer.parseInt(scanner.nextLine());
+//            System.out.print("You entered: " + userGrade + "\n");
+//
+//            if (answer.) {
+//                System.out.print("You got an A!\n");
+//            } else if (userGrade >= 80) {
+//                System.out.print("You got a B!\n");
+//            } else if (userGrade >= 67) {
+//                System.out.print("You got a C.\n");
+//            } else if (userGrade >= 60) {
+//                System.out.print("Well, you got a D.\n");
+//            } else {
+//                System.out.print("It's an F, sorry bud.\n");
+//            }
+//
+//            System.out.print("Would you like to continue: Yes or No? ");
+//            answer = scanner.nextLine();
+//
+//        } while (answer.equalsIgnoreCase("Yes"));
+
+    }
+}
