305aeab2b375449f6e5c45ec71ad6f45de805889
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-06 11:57:47 -0500

    completed question 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        getInteger(1, 10);
 //        System.out.println(addition(2, 2));
 //        System.out.println(subtraction(4, 2));
 //        System.out.println(multiplication(5, 2));
 //        System.out.println(division(10, 2));
 //        System.out.println(modulus(11, 3));
-        System.out.print("Enter a number between 1 and 10: ");
 
-        System.out.println(getInteger(1, 10));
 
+//        System.out.println(getInteger());
+//        getInteger();
     }
 
     // 1
@@ -35,15 +38,20 @@ public class MethodsExercises {
         return x % y;
     }
 
+
     // 2
-    public static int getInteger(int min, int max) {       Scanner scanner = new Scanner(System.in);
-       int userInput = scanner.nextInt();
-       return userInput;
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10: ");
+        int userInput = Integer.parseInt(scanner.nextLine());
 
+        // how to stop
+        if (userInput < min || userInput > max) {
+            System.out.println("You entered invalid number: " + userInput);
+            return getInteger(1, 10);
+        } else {
+            return userInput;
+        }
     }
-
-
-
-
 
 }
