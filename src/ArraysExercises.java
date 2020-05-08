da0bdf92f69911eeca2e1a5839a273fa07c921b1
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-08 16:42:01 -0500

    completed loop to iterate through names

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+
+
+
 public class ArraysExercises {
-    public static void main(String[] args){
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
+    private String name;
+
+    public ArraysExercises() {
 
     }
-}
+
+
+    public static String[] addPerson() {
+        String[] persons = new String[3];
+
+        persons[0] = "Moriah";
+        persons[1] = "Andrew";
+        persons[2] = "Iggy";
+
+
+        for (String person : persons) {
+            System.out.println("Iterated: " + person);
+        }
+        return persons;
+    }
+        public static void main(String[] args){
+//            int[] numbers = {1, 2, 3, 4, 5};
+//            System.out.println(Arrays.toString(numbers
+//            ));
+            System.out.println(Arrays.toString(addPerson()));
+
+
+        }
+    }
+
