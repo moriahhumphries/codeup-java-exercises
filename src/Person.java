9c98275fe464c2589e8df86f6a425024d6adae21
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-08 11:42:27 -0500

    added input class, started exercise 3

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
     private String name;
 
+    public Person(String name){
+        this.name = name;
+    }
+
     // returns the person's name
     public String getName() {
         return this.name;
@@ -12,15 +16,32 @@ public class Person {
     }
     // prints a message to the console using the person's name
     public void sayHello() {
-        System.out.println("Hello, my name is:  " + name);
+        System.out.println("Hello, my name is:  " + getName());
     }
 
     public static void main(String[] args) {
-        Person testPerson = new Person();
-        testPerson.setName("Moriah");
-        System.out.println(testPerson);
+        Person Moriah = new Person("Moriah");
+        Moriah.setName("Moriah");
+        System.out.println(Moriah.getName());
+        Moriah.sayHello();
 
+        // 2
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
 
     }
 
---
+++ src/util/Input.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getString() {
+        System.out.println("Enter a sentence: ");
+        return scanner.nextLine();
+
+    }
+
+    public boolean yesNo(){
+        System.out.println("Enter yes or no");
+        String answer = scanner.nextLine();
+        return answer.equals("Yes") ? true : false;
+    }
+}
