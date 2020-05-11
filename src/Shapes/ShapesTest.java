59e1080d1169392d4c159e82a76af2b533d60646
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-11 15:29:50 -0500

    updated side parameters

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -3,9 +3,13 @@ package Shapes;
 public class ShapesTest {
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("Box1: " + box1.getArea(5, 4));
-        System.out.println("Box1: " + box1.getPerimeter(5, 4));
-//
+        System.out.println("Box 1 area: " + box1.getArea(5, 4));
+        System.out.println("Box 1 perimeter: " + box1.getPerimeter(5, 4));
+
+        Rectangle box2 = new Square(5);
+        System.out.println("Box 2 area: " + box2.getArea(5, 4));
+        System.out.println("Box 2 perimeter: " + box2.getPerimeter(5, 4));
+
 
     }
 }
--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -2,16 +2,25 @@ package Shapes;
 
 public class Square extends Rectangle {
 
+    private int side;
+
     public Square(int side){
-
+    this.side = side;
     }
 
-    public Square(int length, int width) {
-        super(length, width);
-        int side = length + width;
+//    public Square(int length, int width, int side) {
+//        super(length, width);
+//
+//    }
+    public int getArea(int side){
+        return this.side = side ^ 2;
+//        return side ^ 2;
     }
 
+    public int getPerimeter(int side) {
+        return this.side = 4 * side;
+//        return 4 * side;
 
 
-
+    }
 }
