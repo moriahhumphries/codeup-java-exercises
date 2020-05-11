bbc01865cc72f10ec938701e9d9828401e5b91b1
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-11 09:38:32 -0500

    moved methods around in arraysexercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,28 +6,47 @@ public class ArraysExercises {
     public ArraysExercises() {
     }
 
-    public static String[] personArray() {
-        String[] persons = new String[3];
+//    public static String[] personArray() {
+//        String[] persons = new String[3];
+//
+//        persons[0] = "Moriah";
+//        persons[1] = "Andrew";
+//        persons[2] = "Iggy";
+//
+//
+//        for (String person : persons) {
+//            System.out.println("Iterated: " + person);
+//        }
+//        return persons;
+//    }
 
-        persons[0] = "Moriah";
-        persons[1] = "Andrew";
-        persons[2] = "Iggy";
 
-
-        for (String person : persons) {
-            System.out.println("Iterated: " + person);
-        }
-        return persons;
-    }
-
-    public static addPerson(){
-
-    }
-        public static void main(String[] args){
+    public static void main(String[] args) {
 //            int[] numbers = {1, 2, 3, 4, 5};
 //            System.out.println(Arrays.toString(numbers
 //            ));
-            System.out.println(Arrays.toString(personArray()));
+//            System.out.println(Arrays.toString(personArray()));
+        Person[] people = new Person[3];
+
+        people[0] = new Person("Moriah");
+        people[1] = new Person("Andrew");
+        people[2] = new Person("Iggy");
+
+
+        for (Person person: people) {
+            System.out.println("Iterated: " + Arrays.toString(people));
         }
     }
+
+    public static Person[] addPerson(Person[] people, Person person) {
+        Person[] newPeopleArray = new Person[people.length + 1];
+
+        for(int i = 0; i < people.length; i++){
+            newPeopleArray[i] = people[i];
+        }
+        newPeopleArray[newPeopleArray.length - 1] = person;
+        return newPeopleArray;
+    }
+}
+
 
