795b63126294a0bb3b9fb3ba9bdaeb370d26a602
Moriah Humphries <moriah.humphries@gmail.com>
2020-05-14 09:51:12 -0500

    working on loop for student usernames

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -14,48 +14,20 @@ public class Student {
 
     }
 
-
     public String getName() {
         return this.name;
     }
 
     public void addGrade(int grade) {
-        grades.add(grade);
+        this.grades.add(grade);
     }
 
     public double getGradeAverage() {
-        double total = 0;
+        double sum = 0;
         for (Integer grade : this.grades) {
-            total += grade;
+            sum += grade;
         }
-        return total / this.grades.size();
+        return sum / this.grades.size();
     }
-
-
-    public static void main(String[] args) {
-        ArrayList<String> Students = new ArrayList<>();
-        Student Moriah = new Student("Moriah");
-        Student Andrew = new Student("Andrew");
-        Student Iggy = new Student("Iggy");
-        Students.add("Moriah");
-        Students.add("Andrew");
-        Students.add("Iggy");
-        Moriah.addGrade(98);
-        Moriah.addGrade(50);
-        Moriah.addGrade(86);
-        Andrew.addGrade(96);
-        Andrew.addGrade(97);
-        Andrew.addGrade(82);
-        Iggy.addGrade(100);
-        Iggy.addGrade(99);
-        Iggy.addGrade(98);
-        System.out.println("Student Name: " + Moriah.getName());
-        System.out.println(Moriah.getGradeAverage());
-//        for (Student Student : students.values()) {
-//            System.out.println("person.getFullName() = " + Student.getName());
-
-
-        }
-
     }
 
